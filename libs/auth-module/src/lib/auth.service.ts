fef22f680d76e0171e0aa38b1ac56ff262784ad2
Avi Siboni <avisgu@gmail.com>
2022-03-20 21:26:09 +0200

    Adding auth module

---
+++ libs/auth-module/src/lib/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService{
+    constructor(){}
+
+    public isAuthenticated():boolean{
+        return Math.floor(Math.random() * 100) % 2===0;
+    }
+}
